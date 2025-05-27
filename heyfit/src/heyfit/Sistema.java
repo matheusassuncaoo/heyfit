4f8d33d591ce858a50a5ee638656e8ffc5af1ae3
Matheus Assunção da Cunha <106777600+matheusassuncaoo@users.noreply.github.com>
2025-05-26 20:43:47 -0400

    Update Sistema.java

--- heyfit/src/heyfit/Sistema.java
+++ heyfit/src/heyfit/Sistema.java
@@ -4,7 +4,12 @@ public class Sistema{
 	public static void main (String args[])
 	{
 		Menu menu = new Menu();
+		BancoDados bancoDados = new BancoDados();
+		BancoDados.getConexao();
+		
+		//ao iniciar exiba o primeiro menu
 		menu.ExibirMenuInicial();
+		
 
 	}
 }
